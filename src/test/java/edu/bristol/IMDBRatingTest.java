ca2055f9ae92819ac60bc00024ff758cc82099b8
Riley Lee <rileylyw@gmail.com>
2022-02-21 17:38:52 +0000

    test

--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -16,5 +16,12 @@ public class IMDBRatingTest
 
         averageRating = rater.addNewRating(4);
         assertTrue(averageRating == 3.0, "Adding 2nd rating: average should be 3.0");
+
+        averageRating = rater.addNewRating(6);
+        assertTrue(averageRating == 4.0);
+
+        averageRating = rater.addNewRating(8);
+        assertTrue(averageRating == 5.0);
+        //testing branch
     }
 }
